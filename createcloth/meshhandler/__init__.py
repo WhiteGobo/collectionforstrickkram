ab116f89797270a060872c9c91c8ac83a5da93f4
WhiteGobo <richardfechner@posteo.net>
2021-06-28 18:34:27 +0200

    removed not longer used files

--- createcloth/meshhandler/__init__.py
+++
@@ -1,8 +0,0 @@
-from .main import relax_strickgraph_on_surface
-from .create_surfacemap import get_surfacemap
-from .main import weave_positiongraph_into_strickgraph
-from .gridrelaxator import gridrelaxator
-from .main import prepare_gridcopy
-
-#from .surfacemaps_from_ply import create_surfacemap_from_ply \
-#                                    as surfacemap_from_ply_with_xyzuv
--- createcloth/meshhandler/ply_handler/__init__.py
+++
@@ -1,3 +0,0 @@
-from .myimport_ply import load_ply_obj_from_filename
-from .myexport_ply import export_plyfile
-from .datacontainer import ObjectSpec
