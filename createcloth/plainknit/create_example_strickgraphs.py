6a75db23cd4f4063e368932b25c673c2840186e6
WhiteGobo <richardfechner@posteo.net>
2021-07-08 13:01:01 +0200

    asd

--- createcloth/plainknit/create_example_strickgraphs.py
+++ createcloth/plainknit/create_example_strickgraphs.py
@@ -13,7 +13,8 @@ def method1( pairlist: Iterable[ Tuple[ str, str ]], \
 
 from .state import start, end, leftplane, rightplane, lefteaves, righteaves, \
                                     enddecrease, plain, increase, decrease
-def create_example_strickset( verbessererlist, strickgraphsize ):
+def create_example_strickset( verbessererlist, strickgraphsize, \
+                            min_row_length=10 ):
     if strickgraphsize < 3:
         raise Exception( "strickgraphsize must be at least 3" )
     available_startrows = ( start, )
@@ -25,10 +26,19 @@ def create_example_strickset( verbessererlist, strickgraphsize ):
     possible_rowtypelist = it.product( available_startrows, *middlerows, \
                                                         available_endrows )
     from .method_isplain import isplain
+    brulist = []
+    graphs_to_rowdifference = {}
     for rowtypelist in possible_rowtypelist:
         if isplain( rowtypelist ):
-            print( [ str(q) for q in rowtypelist ] )
-            
+            brulist.append( rowtypelist )
+            difference = [ rowtype.downupdifference for rowtype in rowtypelist ]
+            graphs[ rowtypelist ] = difference
+            #print( [ str(q) for q in rowtypelist ] )
         #strickgraph = foo( rowtypelist )
 
 
+
+def find_to_graph_onedifferencegraphs( maingraph, brulist, min_row_length=10 ):
+    maingraph_row_lengths = [1,2,3]
+    for graphtype in brulist:
+        firstrowlength = maingraph_row_lengths
