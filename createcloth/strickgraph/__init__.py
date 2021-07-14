96681ddbc83c2603fc7731836812dc81794b1e2d
WhiteGobo <richardfechner@posteo.net>
2021-07-14 21:21:01 +0200

    asd

--- createcloth/strickgraph/__init__.py
+++ createcloth/strickgraph/__init__.py
@@ -1,11 +1,10 @@
 """
 Base options for strickgraphs
 """
-#from .strickgraph_toknitmanual import tomanual
 from .strickgraph_base import strickgraph
-#from . import load_stitchinfo as stitchinfo
 from .strickgraph_replacesubgraph \
         import create_pathforhashing, follow_cached_path
+#from .fromknitmanual import BrokenManual
 
 from .constants import handknitting_terms, machine_terms
 from .load_stitchinfo import stitchdatacontainer
