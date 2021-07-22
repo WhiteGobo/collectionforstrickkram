e4a657e5ab098a48ac4626003669b41f1bc62788
WhiteGobo <richardfechner@posteo.net>
2021-07-22 11:11:25 +0200

    removed createcloth.utils

--- createcloth/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from ..meshhandler.create_surfacemap import surfacemap
-from ..meshhandler.randrectangle import randrectangle as rand_pymesh
-from ..meshhandler.create_surfacemap import get_surfacemap
-
